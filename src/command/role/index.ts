e7bff89de3d34dd1ed5b244f5c40631b6cbb6655
Corentin Poupry <corentin.poupry@protonmail.com>
2020-06-22 20:09:34 +0200

    Update index.ts

--- src/command/role/index.ts
+++ src/command/role/index.ts
@@ -3,7 +3,7 @@ import { Member } from './member'
 import { Chrono } from './chrono'
 import { Int } from './int'
 
-const role = [Chrono, Int, Unchrono, Member].map(C => {
+const role = [Int, Member].map(C => {
   const command = new C()
 
   command.group = 'role'
